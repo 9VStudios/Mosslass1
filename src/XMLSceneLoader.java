afaeff0cb266eb0de396ff8632c16efacfff6b10
Joel Clarke <joel.clarke273@gmail.com>
2016-11-11 16:13:16 +0000

    Created XMLSceneLoader class and Test Suite for class

---
+++ src/XMLSceneLoader.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Joel on 11/11/2016.
+ */
+class XMLSceneLoader {
+}
---
+++ test/XMLSceneLoaderTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Joel on 11/11/2016.
+ */
+public class XMLSceneLoaderTest {
+
+    @Before
+    public void setUp() throws Exception {
+        XMLSceneLoader sceneLoader = new XMLSceneLoader();
+    }
+
+}
