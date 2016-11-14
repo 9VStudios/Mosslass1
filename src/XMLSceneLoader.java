a1a66d43bed76c952f94ad702898964e25f575fa
Joel Clarke <joel.clarke273@gmail.com>
2016-11-14 14:37:38 +0000

    XMLSceneLoader get method created, returns new Scene object

--- src/XMLSceneLoader.java
+++ src/XMLSceneLoader.java
@@ -1,5 +1,17 @@
 /**
- * Created by Joel on 11/11/2016.
+ * Created by Joel on 12/11/2016.
  */
 class XMLSceneLoader {
+    private static XMLSceneLoader ourInstance = new XMLSceneLoader();
+
+    public static XMLSceneLoader getInstance() {
+        return ourInstance;
+    }
+
+    private XMLSceneLoader() {
+    }
+
+    public Scene get(int sceneId) {
+        return new Scene();
+    }
 }
--- test/XMLSceneLoaderTest.java
+++ test/XMLSceneLoaderTest.java
@@ -1,4 +1,5 @@
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -6,10 +7,29 @@ import static org.junit.Assert.*;
  * Created by Joel on 11/11/2016.
  */
 public class XMLSceneLoaderTest {
+    private static final int SCENE_ID = 1;
+    private XMLSceneLoader sceneLoader;
 
     @Before
     public void setUp() throws Exception {
-        XMLSceneLoader sceneLoader = new XMLSceneLoader();
+        sceneLoader = XMLSceneLoader.getInstance();
+    }
+
+    // Test 1
+    // Created method get(int).
+    // get(int) returns a new Scene object
+    // Refactor:
+    //
+    /**
+     * Calls get(int)and checks that a Scene object is returned
+     */
+    @Test
+    public void getScene() {
+        Scene scene = sceneLoader.get(SCENE_ID);
+        assertTrue(
+                "TEST 1: Get Scene object with id, should return a Scene Object",
+                scene != null
+        );
     }
 
 }
