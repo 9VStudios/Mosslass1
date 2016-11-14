a6131e3a0448c91c1193739f106bd2a89cdec541
Joel Clarke <joel.clarke273@gmail.com>
2016-11-14 15:38:47 +0000

    Created SimpleFacade class and test suite for said class

---
+++ src/SimpleFacade.java
@@ -0,0 +1,7 @@
+import org.simpleframework.xml.*;
+
+/**
+ * Created by Joel on 14/11/2016.
+ */
+class SimpleFacade {
+}
---
+++ test/SimpleFacadeTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Joel on 14/11/2016.
+ */
+public class SimpleFacadeTest {
+    private SimpleFacade facade;
+
+    @Before
+    public void setUp() throws Exception {
+        facade = new SimpleFacade();
+    }
+}
