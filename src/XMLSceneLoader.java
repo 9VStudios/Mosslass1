89d8afb8d47787ed146e972bc10cf087b75ac059
Joel Clarke <joel.clarke273@gmail.com>
2016-11-17 00:04:13 +0000

    Removed XMLSceneLoader constructor and removed test suite

--- src/XMLSceneLoader.java
+++ src/XMLSceneLoader.java
@@ -1,14 +1,9 @@
-/**
- * Created by Joel on 12/11/2016.
- */
+
 class XMLSceneLoader {
     private static XMLSceneLoader ourInstance = new XMLSceneLoader();
 
     public static XMLSceneLoader getInstance() {
         return ourInstance;
-    }
-
-    private XMLSceneLoader() {
     }
 
     public Scene get(int sceneId) {
