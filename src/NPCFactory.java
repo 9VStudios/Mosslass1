7acf4b4c6e42aeb44d1df5b3f010009a866f8030
Arran France <arranf@gmail.com>
2016-11-21 17:43:16 +0000

    Fix NPCFactory

--- src/NPCFactory.java
+++ src/NPCFactory.java
@@ -1,14 +1,26 @@
-NPCFactory(String npcName) {
-	
-	switch(npcName){
+/**
+ * Created by arran on 21/11/2016.
+ */
+public class NpcFactory {
+    private static NpcFactory ourInstance = new NpcFactory();
 
-		case "Mercy":
-			return new NPC1();
-			break;
+    public static NpcFactory getInstance() {
+        return ourInstance;
+    }
 
-		case "Freddie"
-			return new Freddie();
-			break;
-	}
+    private NpcFactory() {
+    }
 
+    public Npc getNpc(String npcName){
+        switch(npcName){
+
+            case "Mercury":
+                return new ExampleNPC("Mercury");
+
+            case "Freddie":
+                return new ExampleNPC("Freddie");
+            default:
+                throw new NotRealNPCException("Well...");
+        }
+    }
 }
